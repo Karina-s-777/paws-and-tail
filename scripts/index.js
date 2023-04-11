dd604ac5a498fc0045ecf0ee24e71f6f249ddb4c
Kolotilina Daria <colotilina.d@yandex.ru>
2023-04-11 12:24:03 +0200

    'промежуточный вариант'

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
+const listOfChoice = document.querySelector('.gallery__choice');
+const element = document.querySelectorAll('.gallery__element');
 
+function filter() {
+  listOfChoice.addEventListener('click', e => {
+    const targetId = e.target.dataset.id;
+    console.log(targetId);
+
+    switch(targetId) {
+      case 'all':
+        break
+      case 'cat':
+        
+    }
+  })
+}
+
+filter();
